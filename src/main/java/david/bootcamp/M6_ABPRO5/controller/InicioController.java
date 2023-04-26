b0b027bd8b527c747357dcd451a3e894029e7666
Jarod Smith <lbriones.dev@gmail.com>
2023-04-26 10:23:59 -0400

    Añade dependencia log4j

--- src/main/java/david/bootcamp/M6_ABPRO5/controller/InicioController.java
+++ src/main/java/david/bootcamp/M6_ABPRO5/controller/InicioController.java
@@ -7,11 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.LoggerFactory;
 
 @Controller
+@Slf4j
 public class InicioController {
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
+		log.info("Se accedió al ROOT DEL PROYECTO");
 		return new ModelAndView("inicio");
 	}
 }
