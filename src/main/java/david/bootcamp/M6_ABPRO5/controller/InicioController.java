a0c170d46f9bb718c78b9e280ac5344fe89f419b
Jarod Smith <lbriones.dev@gmail.com>
2023-04-28 13:09:13 -0400

    Añade Evidencia

--- src/main/java/david/bootcamp/M6_ABPRO5/controller/InicioController.java
+++ src/main/java/david/bootcamp/M6_ABPRO5/controller/InicioController.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.slf4j.LoggerFactory;
 
 @Controller
-@Slf4j
 public class InicioController {
+	
+	private static final Logger logger = Logger.getLogger(InicioController.class);
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
-		log.info("Se accedió al ROOT DEL PROYECTO");
+
 		return new ModelAndView("inicio");
 	}
 }
